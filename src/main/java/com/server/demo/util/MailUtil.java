94eb3a9e860b4934385e1d937f81f5502d569c73
Izzatbek <abdusharipovizzat03@gmail.com>
2025-12-20 16:32:17 +0500

    authentication in progress

--- src/main/java/com/server/demo/util/MailUtil.java
+++ src/main/java/com/server/demo/util/MailUtil.java
@@ -1,4 +1,31 @@
 package com.server.demo.util;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class MailUtil {
+    private final JavaMailSender javaMailSender;
+
+    public void sendMail(String to,String subject,String text){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        javaMailSender.send(message);
+    }
+    public void sendHTMLBody(String to,String subject,String htmlBody) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,"UTF-8");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(htmlBody,true);
+        javaMailSender.send(message);
+    }
 }
